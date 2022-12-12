153f3743c4177b821e90150991e7cbc11977b401
fauzanmd59 <fauzanmd5939@gmail.com>
2022-12-12 19:19:22 +0700

    styling

--- reactsosd/src/App.js
+++ reactsosd/src/App.js
@@ -30,99 +30,6 @@ function App() {
 		fetchData();
 	}, []);
 
-<<<<<<< Updated upstream
-  return (
-    <DataContext.Provider value={{ data, ticket, setTicket, user, setUser }}>
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <ErrorBoundary>
-              <HomePage />
-            </ErrorBoundary>
-          }
-        ></Route>
-        <Route
-          path='/admin'
-          element={
-            <ErrorBoundary>
-              <AdminPage />
-            </ErrorBoundary>
-          }
-        ></Route>
-        <Route
-          path='/admin/login'
-          element={
-            <ErrorBoundary>
-              <AdminLoginPage />
-            </ErrorBoundary>
-          }
-        ></Route>
-        <Route
-          path='/admin/register'
-          element={
-            <ErrorBoundary>
-              <AdminRegisterPage />
-            </ErrorBoundary>
-          }
-        ></Route>
-        <Route
-          path='/search-ticket/ticket-:id'
-          element={
-            <ErrorBoundary>
-              <FormPage />
-            </ErrorBoundary>
-          }
-        ></Route>
-        <Route
-          path='/search-ticket/ticket/payment-:id'
-          element={
-            <ErrorBoundary>
-              <PaymentPage />
-            </ErrorBoundary>
-          }
-        ></Route>
-        <Route
-          path='/about'
-          element={
-            <ErrorBoundary>
-              <AboutPage />
-            </ErrorBoundary>
-          }
-        ></Route>
-        <Route
-          path='/login'
-          element={
-            <ErrorBoundary>
-              <LoginPage />
-            </ErrorBoundary>
-          }
-        ></Route>
-        <Route
-          path='/register'
-          element={
-            <ErrorBoundary>
-              <RegisterPage />
-            </ErrorBoundary>
-          }
-        ></Route>
-        <Route
-          path='/history'
-          element={
-            <ErrorBoundary>
-              {' '}
-              <HistoryPage />
-            </ErrorBoundary>
-          }
-        ></Route>
-        <Route
-          path='/invoicedetail-:id'
-          element={<InvoiceDetailPage />}
-        ></Route>
-      </Routes>
-    </DataContext.Provider>
-  );
-=======
 	return (
 		<DataContext.Provider value={{ data, ticket, setTicket, user, setUser }}>
 			<Routes>
@@ -142,7 +49,6 @@ function App() {
 			</Routes>
 		</DataContext.Provider>
 	);
->>>>>>> Stashed changes
 }
 
 export default App;
