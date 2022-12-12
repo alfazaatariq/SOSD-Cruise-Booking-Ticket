bcefbfdbc688c67df926c0b4ca16be88eed0b275
alfazaatariq <m.alfazaatariq@gmail.com>
2022-12-12 13:27:42 +0700

    rapih2

--- BackendSosd/routes/ticket.js
+++ BackendSosd/routes/ticket.js
@@ -5,8 +5,11 @@ const TicketController = require('../controllers/TicketController');
 const upload = require('../middleware/upload');
 const authenticate = require('../middleware/authenticate');
 
+// AUTHORIZATION
 router.get('/', authenticate, TicketController.index);
+// NON-AUTHORIZATION
 router.get('/getall', TicketController.index);
+
 router.post('/show', TicketController.show);
 router.post('/store', TicketController.store);
 router.post('/update', TicketController.update);
